4af351acdc2c5200fa5d1c8b54a0b5badeaf4d9b
SamProf <wsamprof@gmail.com>
2018-11-29 00:18:34 +0100

    inital commit

---
+++ BlazorFiddle.Blazor/BaseBlazorFiddle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+using Microsoft.AspNetCore.Blazor.Layouts;
+using Microsoft.AspNetCore.Blazor.RenderTree;
+using Microsoft.JSInterop;
+
+namespace BlazorFiddle.Blazor
+{
+    public class BaseBlazorFiddle : BlazorComponent
+    {
+        protected ElementRef Ref;
+
+        [Parameter]
+        protected string Code { get; set; }
+
+        [Parameter]
+        protected string Template { get; set; } = null;
+
+        private bool isFirstRender = true;
+
+        protected async override Task OnInitAsync()
+        {
+            await base.OnInitAsync();
+        }
+
+        protected override async Task OnAfterRenderAsync()
+        {
+            if (isFirstRender)
+            {
+                isFirstRender = true;
+                await JSRuntime.Current.InvokeAsync<object>("blazorFiddle.create", Ref, new
+                {
+                    Text = this.Code,
+                    Template = this.Template,
+                });
+            }
+        }
+    }
+}
